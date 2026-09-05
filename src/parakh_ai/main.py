80412bd2f421a7f07ee9028d1a11c2e02f39d1e3
adarsh singh <adsredmi182615@gmail.com>
2026-09-05 22:51:51 +0530

    websocket part completed

--- src/parakh_ai/main.py
+++ src/parakh_ai/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,WebSocket,WebSocketDisconnect
 
 app = FastAPI()
 
@@ -8,4 +8,15 @@ def home():
         'msg':"Hello from Parakh-AI"
     }
 
+@app.websocket("/interview")
+async def interview_start(websocket: WebSocket):
+    await websocket.accept()
+    print("Candidate Connected")
 
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received from candidate: {data}")
+            await websocket.send_text(f"Echo: {data}")
+    except WebSocketDisconnect:
+        print("Candidate Disconnected")
